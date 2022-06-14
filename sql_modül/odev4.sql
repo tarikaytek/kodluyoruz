535fa7957b6272060c53c7ed4a096df9bee8cccd
tarikaytek <46650139+tarikaytek@users.noreply.github.com>
2022-06-14 19:05:53 +0300

    ready2go

--- sql_modül/odev4.sql
+++ sql_modül/odev4.sql
@@ -1,1 +1,36 @@
+#Soru1
+
+SELECT DISTINCT replacement_cost FROM film
+
+#Soru2
+
+SELECT COUNT(DISTINCT replacement_cost) FROM film
+
+#Soru3
+
+##query
+SELECT COUNT(*) FROM film
+WHERE title LIKE 'T%' AND rating = 'G'
+
+##output
+-- 9
+
+#Soru4
+
+##query
+SELECT count(*) FROM country
+WHERE country LIKE '_____'
+
+##output
+-- 13
+
+#Soru5
+
+##query
+SELECT count(*) FROM city
+WHERE city ILIKE '%r'
+
+##output
+--33
+
 
