e60ef1d476c755863533947f307730c1bde29188
tarikaytek <46650139+tarikaytek@users.noreply.github.com>
2022-06-14 20:14:16 +0300

    ready2go

--- sql_modül/odev6.sql
+++ sql_modül/odev6.sql
@@ -1,1 +1,37 @@
+#Soru1
 
+##query
+SELECT AVG(rental_rate) FROM film
+
+##output
+2.9800000000000000
+
+
+#Soru2
+
+##query
+SELECT count(*) FROM film
+WHERE title LIKE 'C%'
+
+##output
+92
+
+
+#Soru3
+
+#query
+SELECT max(length) FROM film
+WHERE rental_rate = 0.99
+
+#output
+184
+
+
+#Soru4
+
+#query
+SELECT COUNT(DISTINCT replacement_cost) FROM film
+WHERE length > 150
+
+#output
+21
