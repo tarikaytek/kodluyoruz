6b40fc41b035138e065b591f43cf53e61c79ac39
tarikaytek <46650139+tarikaytek@users.noreply.github.com>
2022-06-14 20:01:49 +0300

    ready2go

--- sql_modül/odev5.sql
+++ sql_modül/odev5.sql
@@ -1,1 +1,21 @@
+#Soru1
 
+SELECT * FROM film
+WHERE title LIKE '%n'
+ORDER BY length desc
+LIMIT 5;
+
+#Soru2
+
+SELECT * FROM film
+WHERE title LIKE '%n'
+ORDER BY length desc
+OFFSET 5
+LIMIT 5;
+
+#Soru3
+
+SELECT * FROM customer
+WHERE store_id = 1
+ORDER BY last_name DESC 
+LIMIT 4
