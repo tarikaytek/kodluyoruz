6d13fbfde1e578cda689675ae275cc44bf61d939
tarikaytek <46650139+tarikaytek@users.noreply.github.com>
2022-06-14 18:52:16 +0300

    ready2go

--- sql_modül/odev3.sql
+++ sql_modül/odev3.sql
@@ -1,1 +1,19 @@
+#Soru1
 
+SELECT country FROM country
+WHERE country LIKE 'A%a'
+
+#Soru2
+
+SELECT country FROM country
+WHERE country LIKE '_____n'
+
+#Soru3
+
+SELECT title FROM film
+Where title ILIKE '%t%t%t%t%'
+
+#Soru4
+
+SELECT title FROM film
+Where title LIKE '%c' AND length > 90 AND rental_rate = 2.99
