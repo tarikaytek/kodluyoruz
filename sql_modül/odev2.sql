670d74184eee7a7fefabe80bf13239b6307a8554
tarikaytek <46650139+tarikaytek@users.noreply.github.com>
2022-06-14 18:25:19 +0300

    ready2go

--- sql_modül/odev2.sql
+++ sql_modül/odev2.sql
@@ -1,1 +1,15 @@
+#Soru1
+
+SELECT * FROM film
+WHERE replacement_cost BETWEEN 12.99 AND 16.99;
+
+#Soru2
+
+Select first_name, last_name FROM actor
+WHERE first_name in ('Penelope', 'Nick','Ed');
+
+#Soru3
+
+SELECT * FROM film
+WHERE rental_rate IN (0.99, 2.99, 4.99) AND replacement_cost IN (12.99, 15.99, 28.99);
 
