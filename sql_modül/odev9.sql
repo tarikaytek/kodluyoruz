867a81879235841dae2516f06aaeb1093fad87c4
tarikaytek <46650139+tarikaytek@users.noreply.github.com>
2022-06-15 03:23:27 +0300

    ready2go

--- sql_modül/odev9.sql
+++ sql_modül/odev9.sql
@@ -1,1 +1,14 @@
+#Soru1
 
+SELECT city, country FROM city
+JOIN country ON city.country_id = country.country_id
+
+#Soru2
+
+SELECT first_name, last_name FROM customer
+JOIN payment ON customer.customer_id = payment.customer_id
+
+#Soru3
+
+SELECT first_name, last_name FROM customer
+JOIN rental ON customer.customer_id = rental.customer_id
