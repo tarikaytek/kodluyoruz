4692bf6f6f6cf4df64e50eada5b1a937dd8f1a83
tarikaytek <46650139+tarikaytek@users.noreply.github.com>
2022-06-15 13:46:40 +0300

    ready2go

--- sql_modül/odev12.sql
+++ sql_modül/odev12.sql
@@ -28,7 +28,8 @@ replacement_cost = (SELECT MIN(replacement_cost) FROM film)
 
 #Soru4
 
-
-
-
-
+SELECT COUNT(payment_id) AS count_of_purchases, customer.customer_id, customer.first_name, customer.last_name, customer.email
+FROM payment
+LEFT JOIN customer ON customer.customer_id = payment.customer_id
+GROUP BY customer.customer_id
+ORDER BY COUNT(payment_id) DESC
